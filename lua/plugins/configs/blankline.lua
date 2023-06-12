67abc6f157aa8f3ed951fd0aa37afb9e1925acb3
wutong <wutong079@gmail.com>
2023-06-12 23:44:54 +0900

    Fix Indent-blankline cpp message

--- lua/plugins/configs/blankline.lua
+++ lua/plugins/configs/blankline.lua
@@ -1,60 +1,60 @@
 local M = {}
 
-M.config = function ()
-  require("indent_blankline").setup({
-    char = "│",
-    show_first_indent_level = true,
-    filetype_exclude = {
-      "startify",
-      "dashboard",
-      "dotooagenda",
-      "log",
-      "fugitive",
-      "gitcommit",
-      "packer",
-      "vimwiki",
-      "markdown",
-      -- "json",
-      "txt",
-      "vista",
-      "help",
-      "todoist",
-      "NvimTree",
-      "peekaboo",
-      "git",
-      "TelescopePrompt",
-      "undotree",
-      "flutterToolsOutline",
-      "", -- for all buffers without a file type
-    },
-    buftype_exclude = { "terminal", "nofile" },
-    show_trailing_blankline_indent = false,
-    show_current_context_start = true,
-    show_current_context = true,
-    context_patterns = {
-      "class",
-      "function",
-      "method",
-      "block",
-      "list_literal",
-      "selector",
-      "^if",
-      "^table",
-      "if_statement",
-      "while",
-      "for",
-      "type",
-      "var",
-      "import",
-    },
-    space_char_blankline = " ",
-  })
-  -- because lazy load indent-blankline so need readd this autocmd
-  vim.cmd("autocmd CursorMoved * IndentBlanklineRefresh")
+M.config = function()
+	require("indent_blankline").setup({
+		char = "│",
+		show_first_indent_level = true,
+		filetype_exclude = {
+			"startify",
+			"dashboard",
+			"dotooagenda",
+			"log",
+			"fugitive",
+			"gitcommit",
+			"packer",
+			"vimwiki",
+			"markdown",
+			-- "json",
+			"txt",
+			"vista",
+			"help",
+			"todoist",
+			"NvimTree",
+			"peekaboo",
+			"git",
+			"TelescopePrompt",
+			"undotree",
+			"flutterToolsOutline",
+			"", -- for all buffers without a file type
+		},
+		buftype_exclude = { "terminal", "nofile" },
+		show_trailing_blankline_indent = false,
+		show_current_context_start = true,
+		-- show_current_context = true,
+		context_patterns = {
+			"class",
+			"function",
+			"method",
+			"block",
+			"list_literal",
+			"selector",
+			"^if",
+			"^table",
+			"if_statement",
+			"while",
+			"for",
+			"type",
+			"var",
+			"import",
+		},
+		space_char_blankline = " ",
+	})
+	-- because lazy load indent-blankline so need readd this autocmd
+	vim.cmd("autocmd CursorMoved * IndentBlanklineRefresh")
 end
 
-M.setup = function() 
-  M.config()
+M.setup = function()
+	M.config()
 end
 
 return M
