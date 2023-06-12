9c09bc98087851c47a64ab342fbb454304bd3b08
wutong <wutong079@gmail.com>
2023-06-12 23:13:59 +0900

    Fix unknow nvimtree problem

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -1,181 +1,179 @@
 local M = {}
 
 local configs = {
-  auto_reload_on_write = true,
-  disable_netrw = true,
-  hijack_cursor = false,
-  hijack_netrw = true,
-  hijack_unnamed_buffer_when_opening = false,
-  ignore_buffer_on_setup = false,
-  open_on_setup = false,
-  open_on_setup_file = false,
-  open_on_tab = false,
-  sort_by = "name",
-  update_cwd = false,
-  reload_on_bufenter = false,
-  view = {
-    width = 30,
-    hide_root_folder = false,
-    side = "left",
-    preserve_window_proportions = false,
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes",
-    mappings = {
-      custom_only = false,
-      list = {
-        -- user mappings go here
-      },
-    },
-  },
-  renderer = {
-    indent_markers = {
-      enable = false,
-      icons = {
-        corner = "└ ",
-        edge = "│ ",
-        none = "  ",
-      },
-    },
-    icons = {
-      webdev_colors = true,
-      show = {
-        git = false,
-        folder = true,
-        file = true,
-        folder_arrow = true,
-      },
-      glyphs = {
-        default =        "",
-        symlink=        "",
-        git = {
-          unstaged =     "✗",
-          staged =       "✓",
-          unmerged =     "",
-          renamed =      "➜",
-          untracked =    "★",
-          deleted =      "",
-        },
-        folder = {
-          arrow_open =   "",
-          arrow_closed = "",
-          default =      "",
-          open =         "",
-          empty =        "",
-          empty_open =   "",
-          symlink =      "",
-          symlink_open = "",
-        },
-        -- lsp = {
-        --   hint = "",
-        --   info = "",
-        --   warning = "",
-        --   error = "",
-        -- }
-      },
-    },
-    highlight_git = true,
-    root_folder_modifier = ":t",
-    add_trailing = true,
-    group_empty = true,
-    special_files = {
-      ["Cargo.toml"] = true,
-      Makefile = true,
-      ["README.md"] = true,
-      ["readme.md"] = true,
-    },
-  },
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  ignore_ft_on_setup = {},
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
-  diagnostics = {
-    enable = true,
-    show_on_dirs = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  filters = {
-    dotfiles = false,
-    custom = {},
-    exclude = {},
-  },
-  git = {
-    enable = true,
-    ignore = false,
-    timeout = 200,
-  },
-  actions = {
-    use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = false,
-      restrict_above_cwd = false,
-    },
-    open_file = {
-      quit_on_open = false,
-      resize_window = false,
-      window_picker = {
-        enable = true,
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-        exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-          buftype = { "nofile", "terminal", "help" },
-        },
-      },
-    },
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true,
-  },
-  live_filter = {
-    prefix = "[FILTER]: ",
-    always_show_folders = true,
-  },
-  log = {
-    enable = false,
-    truncate = false,
-    types = {
-      all = false,
-      config = false,
-      copy_paste = false,
-      diagnostics = false,
-      git = false,
-      profile = false,
-    },
-  },
+	auto_reload_on_write = true,
+	disable_netrw = true,
+	hijack_cursor = false,
+	hijack_netrw = true,
+	hijack_unnamed_buffer_when_opening = false,
+	ignore_buffer_on_setup = false,
+	open_on_setup = false,
+	open_on_setup_file = false,
+	open_on_tab = false,
+	sort_by = "name",
+	update_cwd = false,
+	reload_on_bufenter = false,
+	view = {
+		width = 30,
+		hide_root_folder = false,
+		side = "left",
+		preserve_window_proportions = false,
+		number = false,
+		relativenumber = false,
+		signcolumn = "yes",
+		mappings = {
+			custom_only = false,
+			list = {
+				-- user mappings go here
+			},
+		},
+	},
+	renderer = {
+		indent_markers = {
+			enable = false,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				none = "  ",
+			},
+		},
+		icons = {
+			webdev_colors = true,
+			-- show = {
+			-- 	git = false,
+			-- 	folder = true,
+			-- 	file = true,
+			-- 	folder_arrow = true,
+			-- },
+			-- glyphs = {
+			-- 	default = "",
+			-- 	symlink = "",
+			-- 	git = {
+			-- 		unstaged = "✗",
+			-- 		staged = "✓",
+			-- 		unmerged = "",
+			-- 		renamed = "➜",
+			-- 		untracked = "★",
+			-- 		deleted = "",
+			-- 	},
+			-- 	folder = {
+			-- 		arrow_open = "",
+			-- 		arrow_closed = "",
+			-- 		default = "",
+			-- 		open = "",
+			-- 		empty = "",
+			-- 		empty_open = "",
+			-- 		symlink = "",
+			-- 		symlink_open = "",
+			-- 	},
+			-- 	-- lsp = {
+			-- 	--   hint = "",
+			-- 	--   info = "",
+			-- 	--   warning = "",
+			-- 	--   error = "",
+			-- 	-- }
+			-- },
+		},
+		-- highlight_git = true,
+		-- root_folder_modifier = ":t",
+		-- add_trailing = true,
+		-- group_empty = true,
+		-- special_files = {
+		-- 	["Cargo.toml"] = true,
+		-- 	Makefile = true,
+		-- 	["README.md"] = true,
+		-- 	["readme.md"] = true,
+		-- },
+	},
+	hijack_directories = {
+		enable = true,
+		auto_open = true,
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	ignore_ft_on_setup = {},
+	system_open = {
+		cmd = nil,
+		args = {},
+	},
+	diagnostics = {
+		enable = true,
+		show_on_dirs = false,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	filters = {
+		dotfiles = false,
+		custom = {},
+		exclude = {},
+	},
+	git = {
+		enable = true,
+		ignore = false,
+		timeout = 200,
+	},
+	actions = {
+		use_system_clipboard = true,
+		change_dir = {
+			enable = true,
+			global = false,
+			restrict_above_cwd = false,
+		},
+		open_file = {
+			quit_on_open = false,
+			resize_window = false,
+			window_picker = {
+				enable = true,
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+					buftype = { "nofile", "terminal", "help" },
+				},
+			},
+		},
+	},
+	trash = {
+		cmd = "trash",
+		require_confirm = true,
+	},
+	live_filter = {
+		prefix = "[FILTER]: ",
+		always_show_folders = true,
+	},
+	log = {
+		enable = false,
+		truncate = false,
+		types = {
+			all = false,
+			config = false,
+			copy_paste = false,
+			diagnostics = false,
+			git = false,
+			profile = false,
+		},
+	},
 } -- END_DEFAULT_OPTS
 
 local options = {
-  highlight_opend_files = 3, -- 0:no highlight(default), 1:highlight icons, 2:highlight names, 3:highlight icons and names
+	highlight_opend_files = 3, -- 0:no highlight(default), 1:highlight icons, 2:highlight names, 3:highlight icons and names
 }
-local new_setup = {
-
-}
+local new_setup = {}
 
 M.setup = function()
-  -- load global options. See :help nvim-tree.options
-  for opt, val in pairs(options) do
-    vim.g["nvim_tree_" .. opt] = val
-  end
-  -- call setup function.
-  require'nvim-tree'.setup(configs)
-  vim.cmd[[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]]
+	-- load global options. See :help nvim-tree.options
+	for opt, val in pairs(options) do
+		vim.g["nvim_tree_" .. opt] = val
+	end
+	-- call setup function.
+	require("nvim-tree").setup(configs)
+	vim.cmd([[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]])
 end
 
 return M
