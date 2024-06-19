591d67b6ce9aa0729d373dc489689c9eacfa55fc
wutong <wutong079@gmail.com>
2024-06-19 17:08:13 +0800

    Add clangformat for .C

--- init.lua
+++ init.lua
@@ -10,3 +10,7 @@ require("lsp.setup")
   -- Bad Apple 
   -- {'vim-denops/denops.vim'},
   -- {'ryoppippi/bad-apple.vim'},
+
+--hi WinSeparator guifg=#3b4252
+vim.cmd([[highlight WinSeparator guifg=#3b4252]])
+-- vim.cmd("Copilot disable")
